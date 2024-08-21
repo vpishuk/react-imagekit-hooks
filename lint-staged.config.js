9740a66d34c94130af3301468ff7074223d29f8c
Victor Pishuk <victor.pishuk@gmail.com>
2024-08-20 23:24:23 -0400

    chore: release 0.0.5
    Release-As: 0.0.4

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,5 @@ export default {
     "./eslint.config.js": ["eslint --fix"],
     "./lint-staged.config.js": ["eslint --fix"],
     "./jest.config.ts": ["eslint --fix"],
-    "./lerna.json": ["eslint --fix"],
     "./src/**/*": ["eslint --fix"]
 };
